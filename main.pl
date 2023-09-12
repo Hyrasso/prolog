bb0df1895a2d1049d5080d0963011b8e8b782fb9
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2023-09-13 00:01:24 +0200

    upto 17

--- main.pl
+++ main.pl
@@ -36,3 +36,67 @@ list_rev([H|T], M, Y) :- prepend(M, H, NM), list_rev(T, NM, Y).
 palin(X) :- rev(X, X).
 
 % p07
+extend(X, [], X).
+extend(E, [H|T], X) :- append(E, H, NE), extend(NE, T, X).
+
+flatten([], []).
+flatten([H|T], X) :- is_list(H), flatten(H, FH), flatten(T, FT), extend(FH, FT, X).
+flatten([H|T], [H|R]) :- flatten(T, R).
+
+% p08
+compress([], []).
+compress([A], [A]).
+compress([A,A|T], [A|R]) :- compress(T, R).
+compress([A,B|T], [A|R]) :- A \= B, compress([B|T], R).
+
+% p09
+pack([], []).
+pack([E], [[E]]).
+pack([A,A|T], [[A|R]|DR]) :- pack([A|T], [R|DR]).
+pack([A,B|T], [[A],[B|R]|DR]) :- A \= B, pack([B|T], [[B|R]|DR]).
+
+% p10
+% ok to check but causes error when trying to generate the encoded list
+rle([], []).
+rle([A], [[1, A]]).
+rle([A,A|T], [[N, A]|R]) :- rle([A|T], [[NN, A]|R]), N is NN + 1.
+rle([A,B|T], [[1, A],[M, B]|R]) :- A \= B, rle([B|T], [[M, B]|R]).
+
+
+
+% p11 / p13 actually
+mrle([], []).
+mrle([A], [A]).
+mrle([A, A], [[2, A]]).
+mrle([A,A,B|T], [[2, A]|R]) :- A \= B, mrle([B|T], R).
+mrle([A,A|T], [[N, A]|R]) :- mrle([A|T], [[NN, A]|R]), N is NN + 1.
+mrle([A,B|T], [A|R]) :- A \= B, mrle([B|T], R).
+
+% p12
+decode_rle([], []).
+decode_rle([X], [X]) :- \+ is_list(X).
+decode_rle([X|R], [X,Y|T]) :- decode_rle(R, [Y|T]), X \= Y, \+ is_list(X).
+decode_rle([[1, X]|R], [X|T]) :- decode_rle(R, T).
+decode_rle([[N, X]|R], [X|T]) :- N > 1, NN is N - 1, decode_rle([[NN, X]|R], T).
+
+% p14
+dup_list([], []).
+dup_list([A|T], [A, A|R]) :- dup_list(T, R).
+
+% p15
+repeat(A, N, B) :- inner_repeat(A, N, N, B).
+
+inner_repeat([], N, N, []).
+inner_repeat([X|T], 1, N, [X|R]) :- inner_repeat(T, N, N, R).
+inner_repeat([X|T], A, N, [X|R]) :- A > 1, A1 is A - 1, inner_repeat([X|T], A1, N, R).
+
+% p16
+drop_every_n(X, K, Y) :- inner_drop_very_n(X, 0, K, Y).
+
+inner_drop_very_n([], _, _, []).
+inner_drop_very_n([_|T], K, K, R) :- inner_drop_very_n(T, 0, K, R).
+inner_drop_very_n([X|T], N, K, [X|R]) :- N < K, NN is N + 1, inner_drop_very_n(T, NN, K, R).
+
+% p17
+split(X, 0, [], X).
+split([X|T], K, [X|R], L2) :- K > 0, KN is K - 1, split(T, KN, R, L2).
